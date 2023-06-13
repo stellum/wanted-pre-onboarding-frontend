1fe0a7a9756e23b967cd342eb800244d00cbe3c5
stellum <stellaloh16@gmail.com>
2023-06-13 14:50:27 +0900

    feat: 이메일 유효성 검사

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import classes from "./SignUpPage.module.css";
+import useInputs from "../lib/hooks/useInputs";
 
 const SignUpPage = () => {
-  return <div>Sign Up</div>;
+  const [signUpData, setSignUpData] = useInputs({ email: "", password: "" });
+  const [emailError, setEmailError] = useState("");
+
+  useEffect(() => {
+    if (!signUpData.email.includes("@")) {
+      setEmailError("이메일에는 @가 포함되어야 합니다.");
+    } else {
+      setEmailError("유효한 이메일 입니다 :)");
+    }
+  }, [signUpData]);
+
+  return (
+    <>
+      <form className={classes.form}>
+        <h1>회원가입</h1>
+        <input
+          name="email"
+          className={classes.input}
+          type="text"
+          placeholder="이메일을 입력해주세요"
+          id="email"
+          value={signUpData.email}
+          onChange={setSignUpData}
+          data-testid="email-input"
+        />
+        {emailError && <div className={classes.error}>{emailError}</div>}
+
+        <input
+          name="password"
+          className={classes.input}
+          type="password"
+          placeholder="패스워드를 입력해주세요"
+          value={signUpData.password}
+          onChange={setSignUpData}
+          data-testid="password-input"
+        />
+
+        <button data-testid="signup-button">회원가입</button>
+      </form>
+    </>
+  );
 };
 
 export default SignUpPage;
