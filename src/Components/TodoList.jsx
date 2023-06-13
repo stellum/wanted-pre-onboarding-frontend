56c745578ceb841708e504f015ea24ff61a71ae7
stellum <stellaloh16@gmail.com>
2023-06-14 00:55:40 +0900

    feat: todo 우측에 수정, 삭제 버튼

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -26,6 +26,8 @@ export const TodoList = ({
         />
         <span>{todo.todo}</span>
       </label>
+      <button data-testid="modify-button">수정</button>
+      <button data-testid="delete-button">삭제</button>
     </li>
   );
 };
